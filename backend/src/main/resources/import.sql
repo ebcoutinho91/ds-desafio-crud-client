6528b6dca0610889a0d4ef580344e134aad3d382
Ewerton Coutinho <eb.coutinho91@gmail.com>
2022-12-20 14:17:54 -0300

    crud desafio class client

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Fëanor', '40641602836', 50.000, '1978-05-21', 5, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Fingonfin', '38918503556', 100.000, '1980-06-25', 3, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Finrod', '987987987987', 56.000, '1991-03-06', 2, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Melian', '987654321565', 30.000, '1199-06-15', 3, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Finwë', '987987987144', 10.000, '1992-11-17', 1, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Finarfin', '654321987', 900.000, '1989-06-25', 10, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Thingol', '95175365478', 500.000, '1969-12-07', 1, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Beren', '15987453257', 178.000, '1993-07-25', 3, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Lúthien', '321456985245', 666.000, '1994-07-07', 2, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Maedhros', '56127789635', 350.000, '1984-12-04', 3, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Turgon', '32547555263', 500.000, '1975-03-18', 5, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Aredhel', '56127789635', 350.000, '1984-12-04', 3, NOW());
+INSERT INTO  tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Olorin', '32547555263', 500.000, '1975-03-18', 5, NOW());
+
+
